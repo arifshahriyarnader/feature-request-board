f9e4ac617181e9a01430dce74a8d5fb9451baad9
Arif Shahriyar Nader <arif.nader95@gmail.com>
2026-01-03 22:09:56 +0600

    add admin middleware

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { AuthPayload } from "../types/express"; // path adjust
+import { AuthPayload } from "../types/express";
 
 export function authenticateToken(
   req: Request,
